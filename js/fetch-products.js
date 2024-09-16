8fbe18a24bffb6c79e0c9af8420a8756ca9fa6fd
rts-Liva <U2451012@rts-365.dk>
2024-09-16 16:52:40 +0200

    add categories and producers

--- js/fetch-products.js
+++ js/fetch-products.js
@@ -7,8 +7,11 @@ console.log(productData);
 
 const productsContainerElement = document.querySelector('#products-main');
 const categoryFilterContainerElement = document.querySelector('#products-filter__categories');
+const ProducerFilterContainerElement = document.querySelector('#shop-by__producer-list');
+const ProducerContainerElement = document.querySelector('#products-producer__list');
 const productGridElement = document.querySelector('#product-display__grid');
 const productFlexElement = document.querySelector('#product-display__flex');
+const itemsElement = document.querySelector('#products-display__items-text')
 
 productGridElement.addEventListener('click', () => {
     productsContainerElement.classList.replace('flex', 'grid')
@@ -17,6 +20,8 @@ productGridElement.addEventListener('click', () => {
 productFlexElement.addEventListener('click', () => {
     productsContainerElement.classList.replace('grid', 'flex')
 })
+
+itemsElement.innerHTML = `${productData.length} Item(s)`
 
 productData.forEach((product) => {
     const productContainer = document.createElement('article');
@@ -33,16 +38,31 @@ productData.forEach((product) => {
     productContainer.classList.add('product');
 })
 
-function productsCategory() {
-    categoryFilterContainerElement.innerHTML = `
-    <li>${productData[0].category}
-    <li>${productData[4].category}
-    <li>${productData[8].category}
-    <li>${productData[12].category}
-    <li>${productData[16].category}
-    <li>${productData[21].category}
-    <li>${productData[24].category}
-    <li>${productData[29].category}
-    `
+const uniqueCategories = [...new Set(productData.map(product => product.category))];
+const uniqueProducers = [...new Set(productData.map(product => product.producer))];
+const limitedProducers = uniqueProducers.slice(0, 10);
+const productsProducersElement = document.querySelector('#products-producer');
+const viewAllProducersBtn = document.querySelector('#products-producer__button');
+const viewLessProducersBtn = document.createElement('button');
+
+function displayLists() {
+    categoryFilterContainerElement.innerHTML = uniqueCategories.map(category => `<li class="filter-item">${category}</li>`).join('');
+    ProducerFilterContainerElement.innerHTML = uniqueProducers.map(producer => `<li class="filter-item">${producer}</li>`).join('');
+    ProducerContainerElement.innerHTML = limitedProducers.map(producer => `<li class="filter-item products-producer__list-item">${producer}</li>`).join('');
 }
-productsCategory()
+
+displayLists();
+
+viewAllProducersBtn.addEventListener('click', () => {
+        ProducerContainerElement.innerHTML = uniqueProducers.map(producer => `<li class="filter-item products-producer__list-item">${producer}</li>`).join('');
+        viewAllProducersBtn.style.display="none"
+        productsProducersElement.appendChild(viewLessProducersBtn)
+        viewLessProducersBtn.innerHTML = 'View less'
+        viewLessProducersBtn.classList.add('products-producer__button')
+})
+
+viewLessProducersBtn.addEventListener('click', () => {
+    displayLists()
+    viewAllProducersBtn.style.display="block"
+    viewLessProducersBtn.style.display="none"
+})
