ec118994cfb64e47d6548ff4a5f59a9de48059b1
Peter <peter.levinsen@gmail.com peter>
2024-09-17 19:10:54 +0200

    single product view page begun

---
+++ single-product-view-page.js
@@ -0,0 +1,37 @@
+const params = new URLSearchParams( window.location.search );
+
+const productname = params.get("name");
+
+const response = await fetch('data.json');
+const data = await response.json();
+
+function getProductByName (name, products) {
+    return products.find(product => product['name'] == name );
+}
+
+const product = getProductByName(productname, data.products);
+
+console.log(product)
+
+function setTextContentByElementId (elementId, TextContent) {
+    const elementInHTML = document.getElementById(elementId);
+
+    elementInHTML.textContent = TextContent;
+}
+
+function setInnerHTMLByElementId (elementId, innerHTML) {
+    const elementInHTML = document.getElementById(elementId);
+
+    elementInHTML.innerHTML = innerHTML;
+}
+
+setInnerHTMLByElementId(
+    'image',
+    `<img src=${product['image']}>`
+);
+
+setTextContentByElementId('name', product['name']);
+setTextContentByElementId('price', product['price']);
+setTextContentByElementId('category', product['category']);
+setTextContentByElementId('description', product['description']);
+setTextContentByElementId('company', product['company']);
