042fe235bb64d3b55827f27d71b02ad7afb4131c
Benjamin1243 <U2486886@rts-365.dk>
2024-09-18 13:52:28 +0200

    changed db.json

--- js-files/fetchProducts.js
+++ js-files/fetchProducts.js
@@ -65,62 +65,47 @@ let mellem500og1000 = 0;
 const params = new URLSearchParams(window.location.search );
 const lessThanPrice = params.get("price")
 const trueCompany = params.get("company")
+const trueCategory = params.get("category")
+let newFetch = params.get("newFetch")
+let order = params.get("_order")
+console.log(order)
+
+//her tjekker vi om der skal sorteres i de forskellige
+if(newFetch == null){
+    console.log("der")
+    newFetch = ""
+    order = ""
+    
+}else{
+    order = "&_order=" + order
+}
+ console.log(order)
+
 //nu fetcher jeg alle prdukterne
+ console.log("ny")
 
-const response =  await fetch("http://localhost:3000/products");
+const response =  await fetch("http://localhost:3000/products" + newFetch + order);
 const data = await response.json();
-
-console.log(trueCompany)
+console.log(response)
+console.log("erer")
 
 data.forEach(product => {
 
     //her tjekker den for om den er over den valgte price eller firma
-     if(lessThanPrice !== null || trueCompany !== null){
-
+     if(lessThanPrice !== null || trueCompany !== null || trueCategory !== null){
+console.log(product.company == trueCompany)
      
-    if(Number(product.price) < Number(lessThanPrice) || product.company == trueCompany){
+    if(Number(product.price) < Number(lessThanPrice) || product.company == trueCompany || product.category == trueCategory ){
        
-    
+    insertProduct(product)
 
-    //her laver jeg en div som skal omkrandse hele prodktet
-    const productDiv = document.createElement("article")
-    productDiv.classList.add("product")
-    divBox.appendChild(productDiv)
-    //nu laver jeg en div der er med til at putte billede ind i en firkant
-    const imageDiv = document.createElement("div")
-    imageDiv.classList.add("product__imageDiv")
-    productDiv.appendChild(imageDiv)
-    //putter imageDiv ind i produtDiv
-    //nu adder jeg billede et
-    const image = document.createElement("img")
-    image.classList.add("product__image")
-    //putter imageDiv ind i produtDiv
-    //for at finde frem til det rigtige billede bliver koden lidt mærkelig men det er fordi at billeder er delt op i forskellige mapper
-    //denne if tjekker om billederne er et array eller ej
-    if(typeof product.image == "object" ){
-        image.setAttribute("src", "images/produktbilleder/" + product.category + "/" + product.image[0])
-    }else{
-    image.setAttribute("src", "images/produktbilleder/" + product.category + "/" + product.image)
-}
-    imageDiv.appendChild(image)
-    //nu kommer vi til produktet navnet
-    const productName = document.createElement("h2")
-    productName.textContent = product.name
-    productName.classList.add("product__name")
-    productDiv.appendChild(productName)
-
-    //nu kommer det til prisen på prduktet
-    const productPrice = document.createElement("p")
-    productPrice.textContent = product.price + "kr."
-    productPrice.classList.add("product__price")
-    productDiv.appendChild(productPrice)
-    //nu tilføjer vi linket til knappen
-    const productLink = document.createElement("a")
-    productLink.textContent = "view more"
-    productLink.classList.add("product__link")
-    productLink.setAttribute("href", "hifi-shop-single-product-view?name=" + product.name)
-    productDiv.appendChild(productLink)
+   
     }}
+
+    //her tjekker den om der er ingen ting i search paramters
+    if(lessThanPrice == null & trueCompany == null & trueCategory  == null){
+        insertProduct(product)
+    }
     //her tjekker vi får priser
     if(product.price < 4000){
         under4000++
@@ -219,7 +204,59 @@ alreadyShown.textContent =  data.length
 //her tager jeg fat i min sotby option input
 const sortBy = document.querySelector("#sortBy")
 sortBy.addEventListener("input", sortByFunc)
-
+//her kører vi en sortby function
 function sortByFunc(){
-    console.log(sortBy.value)
+    window.open("products.html?newFetch=" + sortBy.value, "_self");
+}
+
+
+
+
+
+
+
+
+
+//her functionene der indsætter produktet
+
+function insertProduct(product){
+     //her laver jeg en div som skal omkrandse hele prodktet
+     const productDiv = document.createElement("article")
+     productDiv.classList.add("product")
+     divBox.appendChild(productDiv)
+     //nu laver jeg en div der er med til at putte billede ind i en firkant
+     const imageDiv = document.createElement("div")
+     imageDiv.classList.add("product__imageDiv")
+     productDiv.appendChild(imageDiv)
+     //putter imageDiv ind i produtDiv
+     //nu adder jeg billede et
+     const image = document.createElement("img")
+     image.classList.add("product__image")
+     //putter imageDiv ind i produtDiv
+     //for at finde frem til det rigtige billede bliver koden lidt mærkelig men det er fordi at billeder er delt op i forskellige mapper
+     //denne if tjekker om billederne er et array eller ej
+     if(typeof product.image == "object" ){
+         image.setAttribute("src", "images/produktbilleder/" + product.category + "/" + product.image[0])
+     }else{
+     image.setAttribute("src", "images/produktbilleder/" + product.category + "/" + product.image)
+ }
+     imageDiv.appendChild(image)
+     //nu kommer vi til produktet navnet
+     const productName = document.createElement("h2")
+     productName.textContent = product.name
+     productName.classList.add("product__name")
+     productDiv.appendChild(productName)
+ 
+     //nu kommer det til prisen på prduktet
+     const productPrice = document.createElement("p")
+     productPrice.textContent = product.price + "kr."
+     productPrice.classList.add("product__price")
+     productDiv.appendChild(productPrice)
+     //nu tilføjer vi linket til knappen
+     const productLink = document.createElement("a")
+     productLink.textContent = "view more"
+     productLink.classList.add("product__link")
+     productLink.setAttribute("href", "hifi-shop-single-product-view?name=" + product.name)
+     productDiv.appendChild(productLink)
+
 }
