4bd8d1154d9bbaf896b5e73165077ebf8fe31f98
EmmaRosby <U2457534@rts-365.dk>
2024-09-23 13:23:33 +0200

    add on single-product.js

--- javascript/single-produkt.js
+++ javascript/single-produkt.js
@@ -1,7 +1,14 @@
 const params = new URLSearchParams(window.location.search);
 const selectedProduct = params.get('name');
 
-
+const productCategoryLocation = document.querySelector('#product-category-location');
+const productImage = document.querySelector('#image-product');
+const productName = document.querySelector('#product-name');
+const productBrand = document.querySelector('#product-brand');
+const productPrice = document.querySelector('#product-price');
+const productDescription = document.querySelector('#product-description');
+const productManufacturer = document.querySelector('#table_td-manufacture')
+const productManufacturerLink = document.querySelector('#table_td-manufacture-link')
 
 fetch('db.json')
 .then(function(response){
@@ -12,18 +19,16 @@ fetch('db.json')
     const productArray = productData.items;
     productArray.forEach(function(product, index){
         if(productData.items[index].name == selectedProduct){
+            productCategoryLocation.innerHTML = ` home <span class="color-black">/</span> ${productArray[index].category} <span class="color-black">/ ${productArray[index].name}</span>`;
+            productImage.innerHTML = `<img src="${productArray[index].img}" alt="product" class="single__produkt-left__container-image-product">`;
+            productName.innerHTML = `${productArray[index].name}`;
+            productBrand.innerHTML = `See other ${productArray[index].brand} products`
+            productPrice.innerHTML = `${productArray[index].price}`;
+            productDescription.innerHTML = `${productArray[index].description}`;
+            
+            productManufacturer.innerHTML = ` ${productArray[index].brand}`;
+            productManufacturerLink.innerHTML = `<a href=""> ${productArray[index].brand} </a>`;
 
-
-
-
-
-/*             const productContainer = document.createElement('div');
-            productContainer.innerhtml = 
-            ``
-
-            const mainWrapper = document.querySelector('#single-produkt-main');
-            mainWrapper.appendChild(productContainer);
-            productContainer.classList.add('main__wrapper') */
         }
     })
 })
