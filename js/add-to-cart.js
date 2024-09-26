3121af36cfd8e026204aae60924172e5d20d1aa4
rts-Liva <U2451012@rts-365.dk>
2024-09-26 13:15:52 +0200

    remove useless code

--- js/add-to-cart.js
+++ js/add-to-cart.js
@@ -7,7 +7,6 @@ const productInfoPaypalBtn = document.querySelector('#product-information__paypa
 const productInfoQuantity = document.querySelector('#quantity');
 const productVariantsContainer = document.querySelector('#product-information__variants');
 
-let productQty = 1;
 let productVariant = 'default';
 
 productVariantsContainer.addEventListener('click', (e) => {
@@ -19,10 +18,8 @@ productVariantsContainer.addEventListener('click', (e) => {
 productInfoCart.addEventListener('click', (e) => {
     if (e.target == productInfoBtn || e.target == productInfoPaypalBtn) {
         e.preventDefault()
-        if (productInfoQuantity.value >= 1) {
-            productQty = productInfoQuantity.value;
-            console.log(productQty);
-        }
+        const productQty = productInfoQuantity.value;
+        console.log(productQty);
 
         localStorage.setItem(chosenProduct, `${productQty},${productVariant}`);
     }
