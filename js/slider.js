dfd3e4c1f52eb82cb57b8a69e0919d4fba12fdd3
umara3 <U2486894@rts-365.dk>
2024-09-27 08:34:14 +0200

    add js

--- js/slider.js
+++ js/slider.js
@@ -1,21 +1,24 @@
-
-
-
-
 let slideIndex = 0;
-console.log("befor:");
+
 fetch('db.json')
 
     .then(response => response.json())
     .then(data => {
-        console.log("data:"+data);
+
         const slidesContainer = document.getElementById('slides');
         data.products.forEach(product => {
             const slide = document.createElement('div');
             slide.classList.add('slide');
+
             const img = document.createElement('img');
             img.src = product.image;
+
+            const productName = document.createElement('div');
+            productName.classList.add('product_name');
+            productName.innerHTML = "<br /><h3>"+ product.product_name+"</h3>";
+
             slide.appendChild(img);
+            slide.appendChild(productName);
             slidesContainer.appendChild(slide);
         });
         showSlides(slideIndex);
@@ -33,48 +36,3 @@ function showSlides(n) {
 function changeSlide(n) {
     showSlides(slideIndex += n);
 }
-
-
-
-
-/*let currentIndex = 0;
-let slidesData = [];
-
-fetch('db.json')
-    .then(response => response.json())
-    .then(data => {
-        slidesData = data.db;
-        displaySlides();
-    });
-
-function displaySlides() {
-    const slidesContainer = document.getElementById('slides');
-    slidesContainer.innerHTML = slidesData.map(slide => `
-        <div class="slide">
-            <img src="${slide.url}" alt="${slide.name}">
-            <h3>${slide.name}</h3>
-        </div>
-    `).join('');
-    updateSlidePosition();
-}
-
-function updateSlidePosition() {
-    const slidesContainer = document.getElementById('slides');
-    slidesContainer.style.transform = `translateX(-${currentIndex * 100}%)`;
-}
-
-function prevSlide() {
-    currentIndex = (currentIndex > 0) ? currentIndex - 1 : slidesData.length - 1;
-    updateSlidePosition();
-}
-
-function nextSlide() {
-    currentIndex = (currentIndex < slidesData.length - 1) ? currentIndex + 1 : 0;
-    updateSlidePosition();
-}*/
-
-
-
-
-
-
